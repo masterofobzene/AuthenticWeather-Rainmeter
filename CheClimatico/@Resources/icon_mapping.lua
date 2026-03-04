a0d07220e7ecd66d9c6ad3688298aa673e62919a
masterofobzene <94469116+masterofobzene@users.noreply.github.com>
2026-03-04 16:56:43 +0000

    Add files via upload

---
+++ CheClimatico/@Resources/icon_mapping.lua
@@ -0,0 +1,32 @@
+-- icon_mapping.lua
+function Initialize()
+    -- Define una tabla que asigne códigos de íconos a nombres de archivos de imágenes
+    iconMap = {
+        ["01d"] = "clear_day.png",
+        ["01n"] = "clear_night.png",
+        ["02d"] = "partly_cloudy_day.png",
+        ["02n"] = "partly_cloudy_night.png",
+        ["03d"] = "cloudy.png",
+        ["03n"] = "cloudy.png",
+        ["04d"] = "broken_clouds.png",
+        ["04n"] = "broken_clouds.png",
+        ["09d"] = "shower_rain_day.png",
+        ["09n"] = "shower_rain_night.png",
+        ["10d"] = "rain.png",
+        ["10n"] = "rain.png",
+        ["13d"] = "snow.png",
+        ["13n"] = "snow.png",
+        ["50d"] = "mist.png",
+        ["50n"] = "mist.png",
+		["11d"] = "thunderstorm.png",
+        ["11n"] = "thunderstorm_n.png",
+    }
+end
+
+function Update()
+    local iconCode = SKIN:GetMeasure('Icon'):GetStringValue() -- Suponiendo que el código del ícono está en la medida 'Ícono'
+
+    local iconName = iconMap[iconCode] or "default.png"
+    
+    SKIN:Bang('!SetVariable', 'IconMapping', iconName)
+end
